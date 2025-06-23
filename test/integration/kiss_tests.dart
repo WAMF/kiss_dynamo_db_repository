10b6d0e23544e3febc5fd484537ad5f206dbf389
defuera <denis@devartel.io>
2025-06-24 00:40:51 +0200

    chore: Clean up

--- test/integration/kiss_tests.dart
+++ test/integration/kiss_tests.dart
@@ -1,12 +1,7 @@
-import 'package:test/test.dart';
 import 'package:kiss_repository_tests/kiss_repository_tests.dart';
 
 import 'factories/dynamodb_repository_factory.dart';
 
 void main() {
-  runRepositoryTests(
-    implementationName: 'DynamoDB',
-    factoryProvider: DynamoDBRepositoryFactory.new,
-    cleanup: () {},
-  );
+  runRepositoryTests(implementationName: 'DynamoDB', factoryProvider: DynamoDBRepositoryFactory.new, cleanup: () {});
 }
